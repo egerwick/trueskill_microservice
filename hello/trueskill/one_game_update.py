a60b2665eec01e00d0523e952f7266d0ec1fc756
Gerwick, Dr. Erik <Erik.Gerwick@senacor.com>
2017-07-08 16:04:51 +0200

    removed print

--- hello/trueskill/one_game_update.py
+++ hello/trueskill/one_game_update.py
@@ -5,5 +5,4 @@ def get_rating(data):
     games = extract(data)
     new_ratings = games_in_trueskill(games)
     json_rankings = player_ranking_to_json(new_ratings) 
-    print json_rankings
     return json_rankings
