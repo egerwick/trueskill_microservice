56d151883dec9c3d30e173ce57c9d65015857046
Gerwick, Dr. Erik <Erik.Gerwick@senacor.com>
2017-07-10 14:48:23 +0200

    deleted uneccessary files

--- hello/trueskill/efficient_scraping.py
+++
@@ -1,8 +0,0 @@
-import urllib2
-
-def scrape_games():
-    url = 'https://kickerlytics.herokuapp.com/api/games'
-    req = urllib2.Request(url)
-    response = urllib2.urlopen(req)
-    the_page = response.read()
-    return the_page
